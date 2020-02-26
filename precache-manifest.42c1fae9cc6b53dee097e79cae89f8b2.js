6f3715524e4397a678f5156e395e366409fefd79
Lahchhsin <nishchhalbhardwaj94@gmail.com>
2020-02-26 19:50:57 +0530

    Updates

---
+++ precache-manifest.42c1fae9cc6b53dee097e79cae89f8b2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5f7d2ca8d193819d28ce85cd3733864",
+    "url": "/portfolio-v1/index.html"
+  },
+  {
+    "revision": "04132894df9b68851d2c",
+    "url": "/portfolio-v1/static/css/main.51e1e833.chunk.css"
+  },
+  {
+    "revision": "215404b64e60df43de05",
+    "url": "/portfolio-v1/static/js/2.67c139b2.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/portfolio-v1/static/js/2.67c139b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04132894df9b68851d2c",
+    "url": "/portfolio-v1/static/js/main.4acbc85b.chunk.js"
+  },
+  {
+    "revision": "76115d4bc0146ef8c5ef",
+    "url": "/portfolio-v1/static/js/runtime-main.2f249861.js"
+  },
+  {
+    "revision": "8ec99cd23abbf4271391b0019962ea63",
+    "url": "/portfolio-v1/static/media/Group4.8ec99cd2.png"
+  },
+  {
+    "revision": "84f7fc6e7be8065fd5cb2bf2315d30c4",
+    "url": "/portfolio-v1/static/media/Group6.84f7fc6e.png"
+  },
+  {
+    "revision": "f7cf409d710ade26e29e6a5d91636bc2",
+    "url": "/portfolio-v1/static/media/Group8.f7cf409d.png"
+  },
+  {
+    "revision": "44b1541a96341780b29112665c66ac67",
+    "url": "/portfolio-v1/static/media/Inter-Black.44b1541a.otf"
+  },
+  {
+    "revision": "fc6aff6f40099ca7468f88df9122ae31",
+    "url": "/portfolio-v1/static/media/Inter-Bold.fc6aff6f.otf"
+  },
+  {
+    "revision": "5c522d2004c3279549bd88222823776f",
+    "url": "/portfolio-v1/static/media/Inter-Regular.5c522d20.otf"
+  },
+  {
+    "revision": "7b686275bc54a66e89bf399c694ead50",
+    "url": "/portfolio-v1/static/media/Resume.7b686275.pdf"
+  },
+  {
+    "revision": "fd488bc4053c1a1f6d8a6f181e6ab877",
+    "url": "/portfolio-v1/static/media/wb.fd488bc4.png"
+  }
+]);
